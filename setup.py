22f79dc9f94090ec71b97f7302c7e4556b0e707f
ksamuel <info.ksamuel@gmail.com>
2012-02-13 14:25:22 +0100

    Improved packaging

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author_email='kevin@yeleman.com',
     packages=find_packages(),
     license='GNU General Public License (GPL), Version 2',
-    long_description=open('README.rst').read(),
+    long_description=open('README').read(),
     provides=['tdo'],
     description='Text based but human and VCS friendly task manager',
     url='http://github.com/yeleman/VCSTodo',
@@ -15,9 +15,7 @@ setup(
     include_package_data=True,
     scripts=['bin/tdo'],
     classifiers=[
-        'Development Status :: 0.1',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: GPL2',
         'Programming Language :: Python',
         'Operating System :: OS Independent',
     ],
